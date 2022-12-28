ebfb2820a7b44a7fab80f9fc1e9c46b57cfb7924
usuiat <stripewani@gmail.com>
2022-12-28 23:57:16 +0900

    Add zoomable Text sample.

--- app/src/main/java/net/engawapg/app/zoomable/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/zoomable/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import net.engawapg.lib.zoomable.rememberZoomState
 import net.engawapg.lib.zoomable.zoomable
 
@@ -24,10 +34,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppScreen() {
-    SingleImage(
-        painter = painterResource(id = R.drawable.bird1),
-        modifier = Modifier.fillMaxSize()
-    )
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabTitles = listOf("Single Image", "Text")
+
+    Column {
+        TabRow(
+            selectedTabIndex = tabIndex,
+        ) {
+            tabTitles.forEachIndexed { index, title ->
+                Tab(
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                    text = { Text(text = title, maxLines = 2) },
+                )
+            }
+        }
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+                .clipToBounds()
+        ) {
+            when (tabTitles[tabIndex]) {
+                "Single Image" -> SingleImage(
+                    painter = painterResource(id = R.drawable.bird1),
+                    modifier = Modifier.fillMaxSize()
+                )
+                "Text" -> ZoomableText(
+                    text = "This is Zoomable Text.",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
 }
 
 @Composable
@@ -45,4 +84,21 @@ fun SingleImage(
         contentScale = ContentScale.Fit,
         modifier = modifier.zoomable(zoomState),
     )
+}
+
+@Composable
+fun ZoomableText(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    val zoomState = rememberZoomState(maxScale = 5f)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.zoomable(zoomState)
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+        )
+    }
 }
