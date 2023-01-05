81e6e4eec7eecae66821ae8689f745f978a41a2b
usuiat <stripewani@gmail.com>
2023-01-05 22:41:35 +0900

    Move ZoomState to a separate file.

--- zoomable/src/main/java/net/engawapg/lib/zoomable/Zoomable.kt
+++ zoomable/src/main/java/net/engawapg/lib/zoomable/Zoomable.kt
@@ -16,29 +16,20 @@
 
 package net.engawapg.lib.zoomable
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.DecayAnimationSpec
-import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.foundation.gestures.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChanged
-import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastForEach
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlin.math.abs
 
@@ -126,209 +117,6 @@ private suspend fun PointerInputScope.detectTransformGestures(
             onGestureEnd()
         }
     }
-}
-
-/**
- * A state object that manage scale and offset.
- *
- * @param maxScale The maximum scale of the content.
- * @param contentSize Size of content (i.e. image size.) If Zero, the composable layout size will
- * be used as content size.
- * @param velocityDecay The decay animation spec for fling behaviour.
- */
-@Stable
-class ZoomState(
-    private val maxScale: Float,
-    private var contentSize: Size = Size.Zero,
-    private val velocityDecay: DecayAnimationSpec<Float> = exponentialDecay(),
-) {
-    private var _scale = Animatable(1f).apply {
-        updateBounds(0.9f, maxScale)
-    }
-    /**
-     * The scale of the content.
-     */
-    val scale: Float
-        get() = _scale.value
-
-    private var _offsetX = Animatable(0f)
-    /**
-     * The horizontal offset of the content.
-     */
-    val offsetX: Float
-        get() = _offsetX.value
-
-    private var _offsetY = Animatable(0f)
-    /**
-     * The vertical offset of the content.
-     */
-    val offsetY: Float
-        get() = _offsetY.value
-
-    private var layoutSize = Size.Zero
-    /**
-     * Set composable layout size.
-     *
-     * Basically This function is called from [Modifier.zoomable] only.
-     *
-     * @param size The size of composable layout size.
-     */
-    fun setLayoutSize(size: Size) {
-        layoutSize = size
-        updateFitContentSize()
-    }
-
-    /**
-     * Set the content size.
-     *
-     * @param size The content size, for example an image size in pixel.
-     */
-    fun setContentSize(size: Size) {
-        contentSize = size
-        updateFitContentSize()
-    }
-
-    private var fitContentSize = Size.Zero
-    private fun updateFitContentSize() {
-        if (layoutSize == Size.Zero) {
-            fitContentSize = Size.Zero
-            return
-        }
-
-        if (contentSize == Size.Zero) {
-            fitContentSize = layoutSize
-            return
-        }
-
-        val contentAspectRatio = contentSize.width / contentSize.height
-        val layoutAspectRatio = layoutSize.width / layoutSize.height
-
-        fitContentSize = if (contentAspectRatio > layoutAspectRatio) {
-            contentSize * (layoutSize.width / contentSize.width)
-        } else {
-            contentSize * (layoutSize.height / contentSize.height)
-        }
-    }
-
-    /**
-     * Reset the scale and the offsets.
-     */
-    suspend fun reset() = coroutineScope {
-        launch { _scale.snapTo(1f) }
-        _offsetX.updateBounds(0f, 0f)
-        launch { _offsetX.snapTo(0f) }
-        _offsetY.updateBounds(0f, 0f)
-        launch { _offsetY.snapTo(0f) }
-    }
-
-    private var shouldConsumeEvent: Boolean? = null
-
-    internal fun startGesture() {
-        shouldConsumeEvent = null
-    }
-
-    internal fun canConsumeGesture(pan: Offset, zoom: Float): Boolean {
-        return shouldConsumeEvent ?: run {
-            var consume = true
-            if (zoom == 1f) { // One finger gesture
-                if (scale == 1f) {  // Not zoomed
-                    consume = false
-                } else {
-                    val ratio = (abs(pan.x) / abs(pan.y))
-                    if (ratio > 3) {   // Horizontal drag
-                        if ((pan.x < 0) && (_offsetX.value == _offsetX.lowerBound)) {
-                            // Drag R to L when right edge of the content is shown.
-                            consume = false
-                        }
-                        if ((pan.x > 0) && (_offsetX.value == _offsetX.upperBound)) {
-                            // Drag L to R when left edge of the content is shown.
-                            consume = false
-                        }
-                    }
-                    else if (ratio < 0.33) { // Vertical drag
-                        if ((pan.y < 0) && (_offsetY.value == _offsetY.lowerBound)) {
-                            // Drag bottom to top when bottom edge of the content is shown.
-                            consume = false
-                        }
-                        if ((pan.y > 0) && (_offsetY.value == _offsetY.upperBound)) {
-                            // Drag top to bottom when top edge of the content is shown.
-                            consume = false
-                        }
-                    }
-                }
-            }
-            shouldConsumeEvent = consume
-            consume
-        }
-    }
-
-    private val velocityTracker = VelocityTracker()
-    private var shouldFling = true
-
-    internal suspend fun applyGesture(
-        pan: Offset,
-        zoom: Float,
-        position: Offset,
-        timeMillis: Long
-    ) = coroutineScope {
-        launch {
-            _scale.snapTo(_scale.value * zoom)
-        }
-
-        val boundX = java.lang.Float.max((fitContentSize.width * _scale.value - layoutSize.width), 0f) / 2f
-        _offsetX.updateBounds(-boundX, boundX)
-        launch {
-            _offsetX.snapTo(_offsetX.value + pan.x)
-        }
-
-        val boundY = java.lang.Float.max((fitContentSize.height * _scale.value - layoutSize.height), 0f) / 2f
-        _offsetY.updateBounds(-boundY, boundY)
-        launch {
-            _offsetY.snapTo(_offsetY.value + pan.y)
-        }
-
-        velocityTracker.addPosition(timeMillis, position)
-
-        if (zoom != 1f) {
-            shouldFling = false
-        }
-    }
-
-    internal suspend fun endGesture() = coroutineScope {
-        if (shouldFling) {
-            val velocity = velocityTracker.calculateVelocity()
-            launch {
-                _offsetX.animateDecay(velocity.x, velocityDecay)
-            }
-            launch {
-                _offsetY.animateDecay(velocity.y, velocityDecay)
-            }
-        }
-        shouldFling = true
-
-        if (_scale.value < 1f) {
-            launch {
-                _scale.animateTo(1f)
-            }
-        }
-    }
-}
-
-/**
- * Creates a [ZoomState] that is remembered across compositions.
- *
- * @param maxScale The maximum scale of the content.
- * @param contentSize Size of content (i.e. image size.) If Zero, the composable layout size will
- * be used as content size.
- * @param velocityDecay The decay animation spec for fling behaviour.
- */
-@Composable
-fun rememberZoomState(
-    maxScale: Float = 5f,
-    contentSize: Size = Size.Zero,
-    velocityDecay: DecayAnimationSpec<Float> = exponentialDecay(),
-) = remember {
-    ZoomState(maxScale, contentSize, velocityDecay)
 }
 
 /**
