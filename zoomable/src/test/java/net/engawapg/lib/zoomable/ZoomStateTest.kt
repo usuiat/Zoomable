52a564d797e06da1a46929b3ecbfd4dbe9d16026
usuiat <stripewani@gmail.com>
2023-12-30 17:00:40 +0900

    Add tests.

--- zoomable/src/test/java/net/engawapg/lib/zoomable/ZoomStateTest.kt
+++ zoomable/src/test/java/net/engawapg/lib/zoomable/ZoomStateTest.kt
@@ -1,11 +1,13 @@
 package net.engawapg.lib.zoomable
 
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 class ZoomStateTest {
 
@@ -31,7 +33,6 @@ class ZoomStateTest {
         }
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_enlargeToGreaterThanMax_enlargesToMax() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -42,7 +43,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 5f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_enlargeToMax_enlargesAsSpecified() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -53,7 +53,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 5f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_enlarge_enlargesAsSpecified() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -64,7 +63,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 2f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_shrink_shrinksAsSpecified() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -76,7 +74,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 1f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_shrinkToMin_shrinksToMin() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -86,7 +83,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 0.9f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_shrinkToLessThanMin_shrinksToMin() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -96,7 +92,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 0.9f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_zoomZero_shrinksToMin() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -106,7 +101,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 0.9f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_zoomNegative_shrinksToMin() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -116,7 +110,6 @@ class ZoomStateTest {
         assertEquals(zoomState.scale, 0.9f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_pan_pansAsExpected() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -127,7 +120,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, 30f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_panBeyondLimits_pansToTopLeftEdge() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -138,7 +130,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, 50f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_panBeyondLimits_pansToBottomRightEdge() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -149,7 +140,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, -50f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_enlargeAtTopLeftCorner_enlargesAndShifts() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -161,7 +151,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, 50f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_enlargeAtBottomRightCorner_enlargesAndShifts() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 100f))
@@ -173,7 +162,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, -50f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_contentHeightIsLessThanLayoutHeight_notShiftsVertically() = runTest {
         val zoomState = ZoomState(contentSize = Size(100f, 30f))
@@ -184,7 +172,6 @@ class ZoomStateTest {
         assertEquals(zoomState.offsetY, 0f)
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun applyGesture_contentWidthIsLessThanLayoutWidth_notShiftsHorizontally() = runTest {
         val zoomState = ZoomState(contentSize = Size(30f, 100f))
@@ -193,5 +180,87 @@ class ZoomStateTest {
         zoomState.applyGesture(Offset(50f, 50f), 2f, Offset.Zero, 0)
         assertEquals(zoomState.offsetX, 0f)
         assertEquals(zoomState.offsetY, 50f)
+    }
+}
+
+@RunWith(Parameterized::class)
+class ZoomStateTestPanWillChangeOffset(
+    private val testCase: TestCase
+) {
+    data class TestCase(
+        val name: String,
+        val zoomPosition: Offset,
+        val pan: Offset,
+        val expected: Boolean,
+    ) {
+        override fun toString() = name
+    }
+
+    companion object {
+        val rect = Rect(Offset.Zero, Size(100f, 100f))
+
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun testCases() = listOf(
+            TestCase(
+                name = "Pan right from right edge will change offset",
+                zoomPosition = rect.centerRight,
+                pan = Offset(1f, 0f),
+                expected = true,
+            ),
+            TestCase(
+                name = "Pan right from left edge will not change offset",
+                zoomPosition = rect.centerLeft,
+                pan = Offset(1f, 0f),
+                expected = false,
+            ),
+            TestCase(
+                name = "Pan left from left edge will change offset",
+                zoomPosition = rect.centerLeft,
+                pan = Offset(-1f, 0f),
+                expected = true,
+            ),
+            TestCase(
+                name = "Pan left from right edge will not change offset",
+                zoomPosition = rect.centerRight,
+                pan = Offset(-1f, 0f),
+                expected = false,
+            ),
+            TestCase(
+                name = "Pan up from top edge will change offset",
+                zoomPosition = rect.topCenter,
+                pan = Offset(0f, -1f),
+                expected = true,
+            ),
+            TestCase(
+                name = "Pan up from bottom edge will not change offset",
+                zoomPosition = rect.bottomCenter,
+                pan = Offset(0f, -1f),
+                expected = false,
+            ),
+            TestCase(
+                name = "Pan down from bottom edge will change offset",
+                zoomPosition = rect.bottomCenter,
+                pan = Offset(0f, 1f),
+                expected = true,
+            ),
+            TestCase(
+                name = "Pan down from top edge will not change offset",
+                zoomPosition = rect.topCenter,
+                pan = Offset(0f, 1f),
+                expected = false,
+            ),
+        )
+    }
+
+    @Test
+    fun test_if_pan_from_edge_changes_offset() = runTest {
+        val zoomState = ZoomState(contentSize = rect.size)
+        zoomState.setLayoutSize(rect.size)
+        zoomState.applyGesture(Offset.Zero, 2f, testCase.zoomPosition, 0)
+
+        val result = zoomState.willChangeOffset(testCase.pan)
+
+        assertEquals(result, testCase.expected)
     }
 }
