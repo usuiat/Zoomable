3c34d8200f2de94a2b3a8360a391176a285740a1
T8RIN <oolbp@bk.ru>
2024-01-09 22:20:42 +0300

    gradle migration to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,7 @@
  * limitations under the License.
  */
 
-buildscript {
-    ext {
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
