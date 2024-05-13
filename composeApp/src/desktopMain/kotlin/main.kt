5fe26e40cc53a783c5d47c581737f8eca0e96c7e
usuiat <stripewani@gmail.com>
2024-05-13 22:27:47 +0900

    Use singleWindowApplication

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,3 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import androidx.compose.ui.window.singleWindowApplication
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Zoomable",
-    ) {
-        App()
-    }
-}
+fun main() = singleWindowApplication(title = "Zoomable") { App() }
