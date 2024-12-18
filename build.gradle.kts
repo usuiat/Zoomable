f097ce5bd3c6730784d2fdcfb71064f13ad3ac7a
usuiat <stripewani@gmail.com>
2024-12-19 07:49:28 +0900

    Update to Kotlin 2.1 and CMP 1.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.jetbrains.compose) apply false
+    alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.vanniktech.maven.publish)
     alias(libs.plugins.dokka)
 }
