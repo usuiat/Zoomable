563ec210d7cfc586eabf14003e2004e220ed244a
usuiat <stripewani@gmail.com>
2024-12-22 12:21:06 +0900

    Add padding to support edge-to-edge

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,8 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
@@ -20,36 +22,38 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        var tabIndex by remember { mutableIntStateOf(0) }
-        val samples = remember { samples() }
+        Scaffold { innerPadding ->
+            var tabIndex by remember { mutableIntStateOf(0) }
+            val samples = remember { samples() }
 
-        Column {
-            var onTapMessage by remember { mutableStateOf("") }
-            val onTap = { point: Offset ->
-                onTapMessage = "Tapped @(${point.x}, ${point.y})"
-            }
-            ScrollableTabRow(
-                selectedTabIndex = tabIndex,
-            ) {
-                samples.forEachIndexed { index, sample ->
-                    Tab(
-                        selected = tabIndex == index,
-                        onClick = { tabIndex = index },
-                        text = { Text(text = sample.title) },
-                    )
+            Column(modifier = Modifier.padding(innerPadding)) {
+                var onTapMessage by remember { mutableStateOf("") }
+                val onTap = { point: Offset ->
+                    onTapMessage = "Tapped @(${point.x}, ${point.y})"
                 }
+                ScrollableTabRow(
+                    selectedTabIndex = tabIndex,
+                ) {
+                    samples.forEachIndexed { index, sample ->
+                        Tab(
+                            selected = tabIndex == index,
+                            onClick = { tabIndex = index },
+                            text = { Text(text = sample.title) },
+                        )
+                    }
+                }
+                Surface(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .clipToBounds()
+                ) {
+                    samples[tabIndex].content(onTap)
+                }
+                Text(
+                    text = onTapMessage
+                )
             }
-            Surface(
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .clipToBounds()
-            ) {
-                samples[tabIndex].content(onTap)
-            }
-            Text(
-                text = onTapMessage
-            )
         }
     }
 }
