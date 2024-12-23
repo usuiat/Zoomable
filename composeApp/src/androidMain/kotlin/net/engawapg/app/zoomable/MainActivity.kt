73e9f8d87ef39dbcfeb6dc895ec4e4d446d45bea
usuiat <stripewani@gmail.com>
2024-12-23 22:56:14 +0900

    test

--- composeApp/src/androidMain/kotlin/net/engawapg/app/zoomable/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/net/engawapg/app/zoomable/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            Scaffold {  }
             App()
         }
     }
