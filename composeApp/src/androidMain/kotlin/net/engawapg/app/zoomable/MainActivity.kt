acc3ad90e996ff2a10a33ff6bcc78ca34ff3b44b
usuiat <stripewani@gmail.com>
2024-12-23 23:00:33 +0900

    Revert "test"
    This reverts commit 73e9f8d87ef39dbcfeb6dc895ec4e4d446d45bea.

--- composeApp/src/androidMain/kotlin/net/engawapg/app/zoomable/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/net/engawapg/app/zoomable/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -13,7 +12,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold {  }
             App()
         }
     }
