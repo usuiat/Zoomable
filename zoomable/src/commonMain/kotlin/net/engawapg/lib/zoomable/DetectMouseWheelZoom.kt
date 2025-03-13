d82ffcd58a11ca5d469fc4e34cf3ec9cc36ce4e0
usuiat <stripewani@gmail.com>
2025-03-13 19:40:51 +0900

    Fix scroll to zoom ratio

--- zoomable/src/commonMain/kotlin/net/engawapg/lib/zoomable/DetectMouseWheelZoom.kt
+++ zoomable/src/commonMain/kotlin/net/engawapg/lib/zoomable/DetectMouseWheelZoom.kt
@@ -5,6 +5,10 @@ import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
 
+internal const val SCROLL_TO_ZOOM_RATE = -0.3f
+private const val MIN_ZOOM = 0.66f
+private const val MAX_ZOOM = 1.5f
+
 internal suspend fun PointerInputScope.detectMouseWheelZoom(
     canZoom: (keyboardModifiers: PointerKeyboardModifiers) -> Boolean,
     onZoom: (zoom: Float, position: Offset) -> Unit,
@@ -16,7 +20,7 @@ internal suspend fun PointerInputScope.detectMouseWheelZoom(
         if (!canZoom(event.keyboardModifiers)) continue
 
         val change = event.changes[0]
-        val zoom = 1f - change.scrollDelta.y * 0.1f
+        val zoom = (1f + change.scrollDelta.y * SCROLL_TO_ZOOM_RATE).coerceIn(MIN_ZOOM, MAX_ZOOM)
         if (zoom == 1f) continue
 
         onZoom(zoom, change.position)
